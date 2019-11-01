a8f24fd8582b4d5c1e46b35b37bc1d8e05e3529e
kub <karsten.bolge@laerdal.com>
2019-11-01 14:21:31 +0100

    Added table

--- src/webparts/rossWayWebpart/RossWayWebpartWebPart.ts
+++ src/webparts/rossWayWebpart/RossWayWebpartWebPart.ts
@@ -58,6 +58,30 @@ interface ISPProject {
     Id: string;
 }
 
+enum Phases {
+    Initialization = "Initialization",
+    BasisDesign = "Basis Design",
+    ChallengeDesign = "Challenge Design",
+    DetailledPlanning = "Detailed Planning",
+    DesignOptimization = "Design Optimization",
+    FinalDesign = "Final Design",
+    Execution = "Execution",
+    Learning = "Learning",
+}
+
+enum Deliverables {
+    BorePlanning = "Bore Planning",
+    ContinuousTasks = "Continuous Tasks",
+    Milestones = "Milestones",
+    FormalRequirements = "Formal Requirements",
+    OfficialApprovements = "Official Approvements",
+}
+
+enum Status {
+    Notstarted = "Not started",
+    Approved = "Approved",
+}
+
 export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWayWebpartWebPartProps> {
     private projectsFetched: boolean;
     private projectsOptions: IPropertyPaneDropdownOption[];
@@ -96,17 +120,108 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
 
                 this.fetchLists(this.context.pageContext.web.absoluteUrl + "/_api/Web/Lists(guid'b28e0d0a-548b-4fbc-95f8-fac3b3b44029')/Items?$select=FSObjType,EncodedAbsUrl,FileRef,Id,Deliverables,Phase,Status1&$filter=startswith(FileRef, '/sites/RossManagement/Delte dokumenter/" + this.properties.project + "/')")// /Projekt 11 Demo')")
                     .then((response2) => {
-                        console.log("Svaret er " + JSON.stringify(response2));
-                        //return response.json();
+                        let color: number[][] = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]];
+                        let col: number;
+                        let row: number;
 
-                        let html: string = "Docuements <TABLE><TR><TD>Id</TD><TD>Title</TD><TD>Deliverables</TD><TD>Phase</TD><TD>Status1</TD><TD>Link</TD></TR>";
+                        let htmlUncategorized: string = "<BR><H2>Uncategorized Docuements</H2><TABLE><TR><TD>Title</TD><TD>Deliverables</TD><TD>Phase</TD><TD>Status1</TD></TR>";
                         response2.value.map((list) => {
                             if (list.FSObjType === 0) {
-                                html += `<TR><TD>${list.Id}</TD><TD>${list.Title}</TD><TD>${list.Deliverables}</TD><TD>${list.Phase}</TD><TD>${list.Status1}</TD><TD><a href="${list.EncodedAbsUrl}">${list.FileRef.substr(list.FileRef.lastIndexOf('/') + 1)}</a></TD></TR>`;
+                                col = -1;
+                                if (list.Phase === Phases.Initialization) col = 0;
+                                else if (list.Phase === Phases.BasisDesign) col = 1;
+                                else if (list.Phase === Phases.ChallengeDesign) col = 2;
+                                else if (list.Phase === Phases.DetailledPlanning) col = 3;
+                                else if (list.Phase === Phases.DesignOptimization) col = 4;
+                                else if (list.Phase === Phases.FinalDesign) col = 5;
+                                else if (list.Phase === Phases.Execution) col = 6;
+                                else if (list.Phase === Phases.Learning) col = 7;
+
+                                row = -1;
+                                if (list.Deliverables === Deliverables.BorePlanning) row = 0;
+                                else if (list.Deliverables === Deliverables.ContinuousTasks) row = 1;
+                                else if (list.Deliverables === Deliverables.Milestones) row = 2;
+                                else if (list.Deliverables === Deliverables.FormalRequirements) row = 3;
+                                else if (list.Deliverables === Deliverables.OfficialApprovements) row = 4;
+
+                                if (row !== -1 && col !== -1 && (list.Status1 === Status.Notstarted || list.Status1 === Status.Approved)) {
+                                    if (color[row][col] === 0) {
+                                        if (list.Status1 === Status.Notstarted) color[row][col] = 1;
+                                        else if (list.Status1 === Status.Approved) color[row][col] = 2;
+                                    }
+                                    else if (color[row][col] === 1) {
+                                        if (list.Status1 === Status.Approved) color[row][col] = 3;
+                                    }
+                                    else if (color[row][col] === 2) {
+                                        if (list.Status1 === Status.Notstarted) color[row][col] = 3;
+                                    }
+                                } else {
+                                    htmlUncategorized += `<TR><TD><a href="${list.EncodedAbsUrl}">${list.FileRef.substr(list.FileRef.lastIndexOf('/') + 1)}</a></TD><TD>${list.Deliverables}</TD><TD>${list.Phase}</TD><TD>${list.Status1}</TD></TR>`;
+                                }
                             }
                         });
-                        html += "</TABLE>";
+                        htmlUncategorized += "</TABLE>";
 
+                        let html: string = "";
+
+                        let headerRow: string = "<TABLE><TR><TD><H2>" + this.properties.project + "</H2></TD>";
+                        let headerColor: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+                        let tableBody = "";
+                        for (row = 0; row < 5; row++) {
+                            tableBody += "<TR><TD class=" + styles.tdblue + ">";
+                            if (row === 0) tableBody += Deliverables.BorePlanning;
+                            if (row === 1) tableBody += Deliverables.ContinuousTasks;
+                            if (row === 2) tableBody += Deliverables.Milestones;
+                            if (row === 3) tableBody += Deliverables.FormalRequirements;
+                            if (row === 4) tableBody += Deliverables.OfficialApprovements;
+                            tableBody += "</TD>";
+                            for (col = 0; col < 8; col++) {
+                                if (row === 0) headerColor[col] = color[row][col];
+
+                                if (color[row][col] === 0) {
+                                    tableBody += "<TD class=" + styles.tdgrey + "> </TD>";
+                                    // no document does not change header color
+                                }
+                                else if (color[row][col] === 1) {
+                                    tableBody += "<TD class=" + styles.tdgrey + "> </TD>";
+                                    if (headerColor[col] === 0) headerColor[col] = 1;
+                                    else if (headerColor[col] == 2) headerColor[col] = 3;
+                                }
+                                else if (color[row][col] === 2) {
+                                    tableBody += "<TD class=" + styles.tdgreen + "> </TD>";
+                                    if (headerColor[col] === 0) headerColor[col] = 2;
+                                    else if (headerColor[col] == 1) headerColor[col] = 3;
+                                }
+                                else if (color[row][col] === 3) {
+                                    tableBody += "<TD class=" + styles.tdorange + "> </TD>";
+                                    headerColor[col] = 3;
+                                }
+
+                            }
+                            tableBody += "</TR>";
+                        }
+
+                        for (col = 0; col < 8; col++) {
+                            if (headerColor[col] === 0) headerRow += "<TD class=" + styles.tdgrey + ">";
+                            else if (headerColor[col] === 1) headerRow += "<TD class=" + styles.tdgrey + ">";
+                            else if (headerColor[col] === 2) headerRow += "<TD class=" + styles.tdgreen + ">";
+                            else if (headerColor[col] === 3) headerRow += "<TD class=" + styles.tdorange + ">";
+
+                            if (col === 0) headerRow += Phases.Initialization;
+                            else if (col === 1) headerRow += Phases.BasisDesign;
+                            else if (col === 2) headerRow += Phases.ChallengeDesign;
+                            else if (col === 3) headerRow += Phases.DetailledPlanning;
+                            else if (col === 4) headerRow += Phases.DesignOptimization;
+                            else if (col === 5) headerRow += Phases.FinalDesign;
+                            else if (col === 6) headerRow += Phases.Execution;
+                            else if (col === 7) headerRow += Phases.Learning;
+
+                            headerRow += "</TD>";
+                        }
+
+                        headerRow += "</TR>";
+
+                        html += headerRow + tableBody + "</TABLE>" + htmlUncategorized;
 
 
                         const listContainer: Element = this.domElement.querySelector("#spListItemContainer");
@@ -125,7 +240,6 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
 
         this._getListData().
             then((response) => {
-                console.log("Karsten json ", response);
                 this._renderList(response.value);
             });
     }
@@ -140,7 +254,6 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
 
         this._getListDocument().
             then((response) => {
-                console.log("Karsten2 json ", response);
                 this._renderListDocument(response.value);
             });
     }
@@ -164,11 +277,30 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
     }
 
     private _renderListDocument(items: ISPListDocument[]): void {
-        console.log("KarsteN");
+        let color: number[][];
+        let col: number;
+        let row: number;
 
-        let html: string = "Docuements <TABLE><TR><TD>Id</TD><TD>Title</TD><TD>Deliverables</TD><TD>Phase</TD><TD>Status1</TD><TD>Link</TD></TR>";
+        let html: string = "Docuements <TABLE><TR><TD>Id</TD><TD>Title</TD><TD>Deliverables</TD><TD>Phase</TD><TD>Status1</TD><TD>Link</TD><TD>col</TD><TD>row</TD></TR>";
         items.forEach((item: ISPListDocument) => {
-            html += `<TR><TD>${item.Id}</TD><TD>${item.Title}</TD><TD>${item.Deliverables}</TD><TD>${item.Phase}</TD><TD>${item.Status1}</TD><TD><a href="${item.ServerRedirectedEmbedUrl}">link</a></TD></TR>`;
+            col = -1;
+            if (item.Phase === Phases.Initialization) col = 0;
+            if (item.Phase === Phases.BasisDesign) col = 1;
+            if (item.Phase === Phases.ChallengeDesign) col = 2;
+            if (item.Phase === Phases.DetailledPlanning) col = 3;
+            if (item.Phase === Phases.DesignOptimization) col = 4;
+            if (item.Phase === Phases.FinalDesign) col = 5;
+            if (item.Phase === Phases.Execution) col = 6;
+            if (item.Phase === Phases.Learning) col = 7;
+
+            row = -1;
+            if (item.Phase === Deliverables.BorePlanning) row = 0;
+            if (item.Phase === Deliverables.ContinuousTasks) row = 1;
+            if (item.Phase === Deliverables.Milestones) row = 2;
+            if (item.Phase === Deliverables.FormalRequirements) row = 3;
+            if (item.Phase === Deliverables.OfficialApprovements) row = 4;
+
+            html += `<TR><TD>${item.Id}</TD><TD>${item.Title}</TD><TD>${item.Deliverables}</TD><TD>${item.Phase}</TD><TD>${item.Status1}</TD><TD><a href="${item.ServerRedirectedEmbedUrl}">link</a></TD><TD>${col}</TD><TD>${row}</TD></TR>`;
         });
 
         html += "</TABLE>";
@@ -181,7 +313,7 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
 
     private _renderDataList(items: ISPListData[]): void {
 
-        let html: string = "<TABLE><TR>";
+        let html: string = "KARSTEN !!!<TABLE><TR>";
         items.forEach((item: ISPListData) => {
             if (item.Status === "Approved") {
                 html += `<TD class="${styles.tdgreen}">${item.Title}</TD>`;
@@ -235,7 +367,6 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
         return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + "/_api/Web/Lists(guid'b28e0d0a-548b-4fbc-95f8-fac3b3b44029')/Items?$select=*,EncodedAbsUrl,FileRef,Id,Deliverables,Phase,Status1&$filter=FSObjType eq 1", SPHttpClient.configurations.v1)
 
             .then((response: SPHttpClientResponse) => {
-                console.log("Svar" + response.status);
                 return response.json();
             });
     }
@@ -257,7 +388,7 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
     public render(): void {
         this.fetchDocumentsGuidAsync();
 
-        this.domElement.innerHTML = `
+        const unused = `
       <div class="${ styles.rossWayWebpart}">
         <div class="${ styles.container}">
           <div class="${ styles.row}">
@@ -272,8 +403,9 @@ export default class RossWayWebpartWebPart extends BaseClientSideWebPart<IRossWa
         </div>
       </div>`;
 
+        this.domElement.innerHTML = `<div class="${styles.rossWayWebpart}"><div id="spListItemContainer"></div></div>`;
         //this._renderListAsync();
-        this._renderListDataAsync();
+        //this._renderListDataAsync();
         this._renderListDocumentAsync();
     }
 
